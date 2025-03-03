0c7f9c21b3886dad54fe57f2aa65aa42d22535a6
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2025-03-03 20:00:40 +0530

    INITIALL

--- bot.py
+++ bot.py
@@ -1,25 +1,24 @@
+from pyrogram import Client
+from commands import register_commands
 import os
 from dotenv import load_dotenv
-from pyrogram import Client
 
-# Load environment variables
 load_dotenv()
 
-API_ID = os.getenv("API_ID")
+# Convert API_ID to int
+API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Ensure API_ID is an integer
-if not API_ID or not API_HASH or not BOT_TOKEN:
-    raise ValueError("❌ Missing environment variables! Check .env or Docker settings.")
-
 app = Client(
     "bot",
-    api_id=int(API_ID),
+    api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
 
+register_commands(app)
+
 if __name__ == "__main__":
-    print("✅ Bot is running...")
+    print("Bot is running...")
     app.run()
