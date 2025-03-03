8c8774133dee92f09b67df7c85216cba59666ce2
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2025-03-03 20:33:30 +0530

    initial

--- bot.py
+++ bot.py
@@ -1,16 +1,19 @@
 from pyrogram import Client
-from commands import register_commands
-import os
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
+api_id = os.getenv("API_ID")
+api_hash = os.getenv("API_HASH")
+bot_token = os.getenv("BOT_TOKEN")
+
 app = Client(
     "bot",
-    bot_token=os.getenv("BOT_TOKEN")
+    api_id=api_id,
+    api_hash=api_hash,
+    bot_token=bot_token
 )
-
-register_commands(app)
 
 if __name__ == "__main__":
     print("Bot is running...")
