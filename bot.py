6ab541df3a3f1131f789fd31ecb45b63a863caf8
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2025-03-03 20:23:50 +0530

    Initial

--- bot.py
+++ bot.py
@@ -5,16 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Convert API_ID to int
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 app = Client(
     "bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
+    bot_token=os.getenv("BOT_TOKEN")
 )
 
 register_commands(app)
--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
+API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
